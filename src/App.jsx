0030d33780108d68b94b4112b75cbd6bfb383874
Yamilet Jerez <quieromiwww@gmail.com>
2023-05-10 12:04:15 +0200

    Routes y Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
 return <>
 <Navbar />
 <h1>App</h1>
+<Routes>
+   <Route></Route> 
+</Routes>
 </>
 };
 
