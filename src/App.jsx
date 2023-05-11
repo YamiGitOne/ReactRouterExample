212b83cf18fa901e2049f70305ec31d248369811
Yamilet Jerez <quieromiwww@gmail.com>
2023-05-11 10:53:28 +0200

    configurar rutas 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
+import Inicio from './pages/inicio';
+import Contacto from './pages/contacto';
+import Blog from './pages/blog';
 
 const App = () => {
 return <>
 <Navbar />
 <h1>App</h1>
 <Routes>
-   <Route></Route> 
+   <Route element={ <Inicio /> } path="/"></Route> 
+   <Route element={ <Contacto /> } path="/contacto"></Route> 
+   <Route element={ <Blog /> } path="/blog"></Route>
 </Routes>
 </>
 };
