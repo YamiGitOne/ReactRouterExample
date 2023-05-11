bd3de1bf6197c1b836e0ef3db33978b6cfc3cb8b
Yamilet Jerez <quieromiwww@gmail.com>
2023-05-11 11:51:41 +0200

    Layout de react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Inicio from './pages/inicio';
 import Contacto from './pages/contacto';
 import Blog from './pages/blog';
+import LayoutPublic from "./Layouts/LayoutPublic";
 
 const App = () => {
 return <>
 <Navbar />
-<h1>App</h1>
-<Routes>
-   <Route element={ <Inicio /> } path="/"></Route> 
-   <Route element={ <Contacto /> } path="/contacto"></Route> 
-   <Route element={ <Blog /> } path="/blog"></Route>
-</Routes>
+   <Routes>
+      <Route path="/" element={ < LayoutPublic/>}>
+         <Route element={ <Inicio /> } index></Route> 
+         <Route element={ <Contacto /> } path="/contacto"></Route> 
+         <Route element={ <Blog /> } path="/blog"></Route>
+      </Route>
+   </Routes>
 </>
 };
 
