5ba88a75d78da58895d7cb2f3dcf29f861c669d3
Yamilet Jerez <quieromiwww@gmail.com>
2023-05-11 12:28:04 +0200

    componente NavLink + 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
 const NotFound = () =>{
     return (
         <>
         <h1>404</h1>
+        <Link className="btn btn-outline-primary" to="/">Volver al inicio</Link>
         </>
     );
 };
