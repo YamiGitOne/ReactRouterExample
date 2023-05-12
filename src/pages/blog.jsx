8aca27803c752ba52384312585974f85f62d66f2
Yamilet Jerez <quieromiwww@gmail.com>
2023-05-12 09:39:26 +0200

    -rutas con parametros para el blog

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,10 +1,22 @@
+import {Link} from 'react-router-dom';
 import {useFetch} from "../hooks/UseFetch"
 const Blog = () =>{
-    const {} = useFetch('https://jsonplaceholder.typicode.com/posts')
+    const {data, loading, error} = useFetch('https://jsonplaceholder.typicode.com/posts')
+
+    if(loading) return <p>Loading data...</p>
+    if(error) return <p>Error data...</p>
+
     return (
-        <>
+    <>
         <h1>Blog</h1>
-        </>
+        <ul className="list-group">
+            {
+                data.map(item => (
+                    <Link className="list-group-item" to={'/blog/${item.id}'} key={item.id}>{item.id} - {item.title}</Link>
+                ))
+            }
+        </ul>
+    </>
     );
 };
 
