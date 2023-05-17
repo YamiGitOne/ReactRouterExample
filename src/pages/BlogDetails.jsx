64a52f6fde9ad02f0116091c189278112ac887a3
Yamilet Jerez <quieromiwww@gmail.com>
2023-05-17 11:02:59 +0200

    lógica para filtrar 1

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,5 +1,19 @@
+import { useParams, Link } from "react-router-dom";
+import {useFetch} from '../hooks/UseFetch'
 const BlogDetails = () => {
-    return "BlogDetails";
+    const params = useParams();
+   
+    const {data, loading, error} = useFetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+    if(loading) return <p>Loading data...</p>;
+    if(error) return <p>Error data...</p>;
+
+    return (
+        <>
+        <h1>{data.id} - {data.title}</h1>
+        <p>{data.body}</p>
+        <Link to="/blog">Volver a Blogs</Link>
+        </>
+    )
 };
 
 export default BlogDetails;
--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,14 +1,34 @@
-import {Link} from 'react-router-dom';
+import {Link, useSearchParams} from 'react-router-dom';
 import {useFetch} from "../hooks/UseFetch"
+
 const Blog = () =>{
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+
     const {data, loading, error} = useFetch('https://jsonplaceholder.typicode.com/posts')
 
     if(loading) return <p>Loading data...</p>
     if(error) return <p>Error data...</p>
 
+    const handleChange = (e) => {
+        let filter = e.target.value
+        setSearchParams({filter: filter});
+
+       // console.log(e.target.value);
+        //console.log("change");
+    };
+
     return (
     <>
         <h1>Blog</h1>
+        <input 
+        type='text'
+        value={searchParams.get("filter") || ""} 
+        name='' 
+        onChange={handleChange} 
+        className='form-control mb-2' />
+
         <ul className="list-group">
             {
                 data.map(item => (
